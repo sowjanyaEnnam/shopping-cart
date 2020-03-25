4b90e1a50f272050a3f5795ec53f7199d1a2953c
Sowjanya Ennam <sowjanyaennam95@gmail.com>
2020-03-25 22:45:50 +0530

    Updates

---
+++ precache-manifest.efe3f16346fb55e7d73d4a0b7b823a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83e75bcb220070a5b5a88df69cc9d0e",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "11b01e2c3ec7f197fdc4",
+    "url": "/shopping-cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be874cd3921f72f4d091",
+    "url": "/shopping-cart/static/js/2.03dd0919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.03dd0919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b01e2c3ec7f197fdc4",
+    "url": "/shopping-cart/static/js/main.c8754834.chunk.js"
+  },
+  {
+    "revision": "531342bfcad1931b43d0",
+    "url": "/shopping-cart/static/js/runtime-main.d30a4a8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
